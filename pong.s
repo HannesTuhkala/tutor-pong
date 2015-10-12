3cc49c79f8c2ac74beac08e421a16a5fdae6b0a6
Hannes Tuhkala <hantu447@bussen-10.edu.isy.liu.se>
2015-10-12 10:03:49 +0200

    Added variables for player 1 and player 2

--- pong.s
+++ pong.s
@@ -1,3 +1,10 @@
+LPOSY		DC.B	0			; left paddle (0..4)
+RPOSY		DC.B	0			; right paddle (0..4)
+BPOSX		DC.B	0			; Ball position column (0..6)
+BPOSY		DC.B	0			; Ball position row (0..4)
+
+MULTPLEXLINE	DC.B	0
+
 COLDSTART:	LEA	$08000, A7		; set stack pointer
 		JSR	PIAINIT			; set up I/O
 
