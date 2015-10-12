1fa4f6358ea7a62649dc23c1e3afcd14d32f9c0d
Hannes Tuhkala <hantu447@bussen-10.edu.isy.liu.se>
2015-10-12 10:07:05 +0200

    Added more comments

--- pong.s
+++ pong.s
@@ -7,11 +7,15 @@ MULTPLEXLINE	DC.B	0
 
 COLDSTART:	LEA	$08000, A7		; set stack pointer
 		JSR	PIAINIT			; set up I/O
+		JSR	INSTALLINTS		; install and enable interrupts
 
 PIAINIT:	CLR.B	$10084			; clear CRA
-		MOVE.B	$0F,$10080
-		MOVE.B	$17,$10084
+		; 0..3 multiplex out
+		; 4,5  left paddle (player 1)
+		; 6,7  right paddle (player 2)
+		MOVE.B	$0F,$10080		; 7..4 in, 3..0 out.
+		MOVE.B	$17,$10084		; allow interrupts only on CA1
 
 		CLR.B	$10086			; clear CRB
-		MOVE.B	$FF,$10082
-		MOVE.B	$16,$10086
+		MOVE.B	$FF,$10082		; 7..0 all out
+		MOVE.B	$16,$10086		; do not allow any interrupts
