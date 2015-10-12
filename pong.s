31a041096b8781e7c7b537f3ca4ce9e3e1fea65c
Hannes Tuhkala <hantu447@bussen-10.edu.isy.liu.se>
2015-10-12 09:58:34 +0200

    Configured PIA INIT and added comments

--- pong.s
+++ pong.s
@@ -1,10 +1,10 @@
-COLDSTART:	LEA		$7000, A7	;set stack pointer
-			JSR		PIAINIT		;set up I/O
+COLDSTART:	LEA	$08000, A7		; set stack pointer
+		JSR	PIAINIT			; set up I/O
 
-PIAINIT:	CLR.B	$10084
+PIAINIT:	CLR.B	$10084			; clear CRA
 		MOVE.B	$0F,$10080
 		MOVE.B	$17,$10084
 
-		CLR.B	$10086
+		CLR.B	$10086			; clear CRB
 		MOVE.B	$FF,$10082
 		MOVE.B	$16,$10086
